61e20692e90c2d440c7d3cc90b2452cb1b44c6da
Thanh Tung <tungtoitiu@gmail.com>
2018-06-25 14:06:54 +0700

    test some syntax of socket.io

--- server/server.js
+++ server/server.js
@@ -3,10 +3,22 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+  console.log('a new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('a user was disconnected');
+  });
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('server is up on ', port);
 });
