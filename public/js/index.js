2ed7997cb2de4223bb4c5459be9fa01727ec8e86
ToBaThanhTung <tungtoitiu@gmail.com>
2018-07-07 20:57:52 +0700

    add autoScroll fun

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessages = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessages.innerHeight();
+  var lastMessageHeight = newMessages.prev().innerHeight();
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', () => {
   console.log('connected to server');
 });
@@ -17,6 +32,7 @@ socket.on('newMessage', (message) =>{
       createAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 
@@ -31,6 +47,7 @@ socket.on('newLocationMessage', (message) => {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
